b2d6a1bee8ec81ed7d0db764863eb28b5750f8a6
Alexandre Junior <alexandrejrdevfull@gmail.com>
2026-07-23 21:36:55 -0300

    feat: estabelecer rotas para entrega de recurso de usuarios

---
+++ server/src/routers/user.router.js
@@ -0,0 +1,11 @@
+import { getAllUsers } from "../controllers/user.controller.js";
+
+export default function routerUser(request, response) {
+
+    if (request.method === 'GET') return getAllUsers(response);
+
+   /*   if (request.method === 'GET') {
+        return getAllUsers(response);
+    } */
+
+}
