d3e1f439aeac8ddd72bdb07d016c2047bd2b1446
Alexandre Junior <alexandrejrdevfull@gmail.com>
2026-07-23 21:29:39 -0300

    refactor: mover dados simulados para diretorio dedicado

---
+++ server/src/mocks/user.mock.js
@@ -0,0 +1,15 @@
+export const users = [{
+    nome: 'Alexandre Junior',
+    age: 39,
+    status: true
+}, {
+    nome: 'Juliana Andrade',
+    age: 38,
+    status: true
+}, {
+    nome: 'Hanna Kely',
+    age: 15,
+    status: true
+}];
+
+export default users;
